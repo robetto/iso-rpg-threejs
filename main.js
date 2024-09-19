7b2bc4d2d9791db8fec4cff8de7af2335d08ea85
robetto <robertobettoni@gmail.com>
2024-09-19 12:52:49 +0200

    prima commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+controls.update();
+
+function animate() {
+
+	controls.update();
+
+	renderer.render(scene, camera);
+
+}
