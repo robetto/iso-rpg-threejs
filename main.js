8164a7635109a92f9a072764ddf8e02ef34145bd
robetto <robertobettoni@gmail.com>
2024-09-19 13:01:32 +0200

    daje

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js'
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -8,10 +12,18 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const sun = new THREE.DirectionalLight()
+sun.position.set(1, 2, 3);
+scene.add(sun)
+
+const ambient = new THREE.AmbientLight();
+ambient.intensity = 0.5;
+scene.add(ambient)
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0xfff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -21,7 +33,7 @@ controls.update();
 function animate() {
 
 	controls.update();
-
+	stats.update()
 	renderer.render(scene, camera);
 
 }
